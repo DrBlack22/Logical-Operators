5fa666bf04e561d3c70edc7ec8fd9fa2e949cb0c
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-10-27 11:50:53 +0500

    First commit

---
+++ Logical Operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int age = 20;
+    int money = 800000;
+
+    if(age > 21 || money > 80){
+
+            cout << "Welcome sire!" << endl;
+
+    }
+}
+
+/* LOGICAL OPERATORS
+
+- && AND logical operator, all conditions separated by AND have to be true.
+
+- || OR logical operator, any of the conditions separated by OR have to be true.
+*/
